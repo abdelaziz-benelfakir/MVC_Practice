3b7a7fb1e715191dca7e1750236e8f077565237c
A.Benelfakir <abdelaziz.benelfakir@talan.com>
2018-09-20 16:57:12 +0200

    Initial Commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Practice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Practice.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) 
+        {
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
